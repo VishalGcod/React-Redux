e685a54bf3d6f473bbefccc119dbad36a19399f8
Ganesan <JGANESA1@ford.com>
2023-08-05 23:53:18 +0530

    saturdays works

--- src/NavData.js
+++ src/NavData.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
+import axios from "axios";
+import { useState } from "react";
+// import { MDBIcon } from "mdb-react-ui-kit";
+
+const Form=styled.form`display:flex;
+flex-direction:column`;
 
 const StyledNavbar = styled.div`
   background-color: #333;
@@ -9,38 +15,74 @@ const StyledNavbar = styled.div`
   justify-content: space-between;
   align-items: center;
   height: 90px;
-  font-weight:600;
+  font-weight: 600;
 `;
 
 const NavLink = styled.a`
   color: #fff;
   text-decoration: none;
   padding: 0.5rem 1rem;
-  font-size:25px;
+  font-size: 25px;
 
   &:hover {
     background-color: #555;
   }
 `;
-export const Data=()=> {
-  return (
-    <div>Data Page</div>
-  )
-}
+export const Data = () => {
+  const [dropDownData, getDropDownData] = useState([]);
+  const [country, setCountry] = useState("");
+  const dropDown = () => {
+    axios
+      .get("http://localhost:8000/countries")
+      .then((res) => {
+        getDropDownData(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-export const About=()=> {
-    return (
-      <div>About Page</div>
-    )
-  }
-  
-  export const Login=()=> {
-    return (
-      <div>Login Page</div>
-    )
-  }
-  
-export const Navbar=()=> {
+  useEffect(() => {
+    dropDown();
+  }, []);
+
+  const setCountryCode = () => {
+    setCountry();
+  };
+
+  return (
+    <div>
+      <Form>
+        <input type="email"></input>
+        <input type="email"></input>
+        one<input type="checkbox" checked="checked"></input>
+        two<input type="checkbox"></input>
+        three<input type="checkbox"></input>
+        <input type="radio" name="fav_language" value="HTML"></input>four
+        <input type="radio" name="fav_language" value="HTML"></input>five
+        <input type="date"></input>
+        <select name="countries" onChange={setCountryCode}>
+          {dropDownData.map((e) => (
+            <option value="countries" selected>
+              <span>{e.dial_code}</span>
+              <span>{e.name}</span>
+              <span>{e.code}</span>
+            </option>
+          ))}
+        </select>
+        <button>Submit</button>
+      </Form>
+    </div>
+  );
+};
+
+export const About = () => {
+  return <div>About Page</div>;
+};
+
+export const Login = () => {
+  return <div>Login Page</div>;
+};
+
+export const Navbar = () => {
   return (
     <StyledNavbar>
       <div>
@@ -61,4 +103,4 @@ export const Navbar=()=> {
       </div>
     </StyledNavbar>
   );
-}
+};
