b198867b9c29a610bc45d009b6fc010524a2afee
Ganesan <JGANESA1@ford.com>
2023-08-06 13:17:37 +0530

    new changes

--- src/NavData.js
+++ src/NavData.js
@@ -8,6 +8,25 @@ import { useState } from "react";
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  border: 2px solid grey;
+  box-shadow: 2px 2px 2px grey;
+  width: 40%;
+  & > input {
+    width: 50%;
+    margin-top: 3%;
+    margin-bottom: 3%;
+  }
+  & > select {
+    width: 50%;
+  }
+`;
+
+const FormDiv = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: 500px;
 `;
 
 const StyledNavbar = styled.div`
@@ -46,33 +65,123 @@ export const Data = () => {
     dropDown();
   }, []);
 
-  const setCountryCode = () => {
-    setCountry();
+  const [array, setarray] = useState([]);
+  const [options,setOptions]=useState('')
+  const [formDatas, setFormDatas] = useState({
+    email: "",
+    name: "",
+    date: "",
+    num: "",
+    check1: "",
+    check2: "",
+    check3: "",
+    // countries:"",
+  });
+
+  const valueChange = (e) => {
+    const { name, value } = e.target;
+    setFormDatas({ ...formDatas, [name]: value });
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setarray([...array, formDatas]);
+    console.log(options);
   };
 
   return (
-    <div>
-      <Form>
-        <input type="email"></input>
-        <input type="email"></input>
-        one<input type="checkbox" checked="checked"></input>
-        two<input type="checkbox"></input>
-        three<input type="checkbox"></input>
-        <input type="radio" name="fav_language" value="HTML"></input>four
-        <input type="radio" name="fav_language" value="HTML"></input>five
-        <input type="date"></input>
-        <select name="countries" onChange={setCountryCode}>
+    <FormDiv>
+      <Form onSubmit={formSubmit}>
+        <input
+          type="email"
+          value={formDatas.email}
+          onChange={valueChange}
+          name="email"
+        ></input>
+        <input
+          type="text"
+          value={formDatas.name}
+          onChange={valueChange}
+          name="name"
+        ></input>
+        <div>
+          one
+          <input
+            type="checkbox"
+            // checked="checked"
+            name="check1"
+            value="one"
+            onChange={valueChange}
+          ></input>
+          two
+          <input
+            type="checkbox"
+            name="check2"
+            value="two"
+            onChange={valueChange}
+          ></input>
+          three
+          <input
+            type="checkbox"
+            name="check3"
+            value="three"
+            onChange={valueChange}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="num"
+            value="four"
+            onChange={valueChange}
+          ></input>
+          four
+          <input
+            type="radio"
+            name="num"
+            value="five"
+            onChange={valueChange}
+          ></input>
+          five
+        </div>
+        <input
+          type="date"
+          name="date"
+          value={formDatas.date}
+          onChange={valueChange}
+        ></input>
+        <select
+          name="countries"
+          onChange={(e)=>setOptions(e.target.value)}
+          value={options}
+        >
           {dropDownData.map((e) => (
-            <option value="countries" selected>
-              <span>{e.dial_code}</span>
+            <option>
+              <span>{e.dial_code} </span>
               <span>{e.name}</span>
-              <span>{e.code}</span>
+              {/* <span>{e.code}</span> */}
             </option>
           ))}
         </select>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </Form>
-    </div>
+      <div>
+        {array.map((e) => (
+          <div>
+          <div>
+            <h3>{e.email}</h3>
+            <h3>{e.name}</h3>
+            <h3>{e.date}</h3>
+            <h3>{e.num}</h3>
+            <h3>
+              {e.check1} {e.check2} {e.check3}
+            </h3>
+          </div>
+          <h3>{options}</h3>
+          </div>
+        ))}
+      </div>
+    </FormDiv>
   );
 };
 
